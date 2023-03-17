ab0592e63cc22ef2a06b09afb89184483c1b4340
Tariq Naseem <tariq.naseem333@gmail.com>
2023-03-17 15:58:02 +0530

    Programming Using Java Certificate

--- privateSectorOperationsAutomationProject/InvalidAssetsException.java
+++
@@ -1,9 +0,0 @@
-package privateSectorOperationsAutomationProject;
-
-public class InvalidAssetsException extends Exception {
-	
-	public InvalidAssetsException( String message ) {
-		super( message );
-	}
-	
-}
